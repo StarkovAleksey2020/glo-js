aaadb343183ca1f1ae5754427813c20fee89eb31
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2021-01-29 15:02:40 +0800

    Added code for the mandatory task of lesson 2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 80000;
+let income = 50000;
+let addExpenses = 'интернет, такси, коммуналка';
+let deposit = true;
+let mission = 12000000;
+let period = 12;
 
-alert('Alert test string');
-console.log('Console test string');
+console.log('typeof money: ', typeof money);
+console.log('typeof income: ', typeof income);
+console.log('typeof deposit: ', typeof deposit);
+
+console.log('addExpenses.length: ', addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log('addExpenses: ', addExpenses);
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
