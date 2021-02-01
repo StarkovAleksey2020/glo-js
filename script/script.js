b07c104e4244563ef7cd7934f61959fc9a7e8e71
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2021-02-01 18:25:52 +0800

    Added code for the mandatory task of lesson 03

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// LESSON 02
 let money = 80000;
 let income = 50000;
 let addExpenses = 'интернет, такси, коммуналка';
@@ -18,3 +19,78 @@ console.log('addExpenses: ', addExpenses);
 
 let budgetDay = money / 30;
 console.log('budgetDay: ', budgetDay);
+
+// LESSON 03
+// Месячный доход
+const MONEY_TITLE = "Ваш месячный доход?";
+let defaultMoney = 80000;
+money = parseInt(prompt(MONEY_TITLE, defaultMoney));
+console.log('money: ', money);
+
+// Расходы
+const EXPENSES_TITLE = "Перечислите возможные расходы за рассчитываемый период через запятую";
+let defaultAddExpenses = "Квартплата, проездной, кредит";
+addExpenses = prompt(EXPENSES_TITLE, defaultAddExpenses);
+console.log('addExpenses: ', addExpenses);
+
+// Наличие депозита
+const DEPOSIT_TITLE = "Есть ли у вас депозит в банке?";
+deposit = confirm(DEPOSIT_TITLE);
+console.log('deposit: ', deposit);
+
+// Статьи расходов и их стоимость
+const EXPENSES_TITLE_1 = "Введите обязательную статью расходов? (1)";
+const EXPENSES_TITLE_2 = "Введите обязательную статью расходов? (2)";
+const AMOUNT_TITLE_1 = "Во сколько это обойдется? (1)";
+const AMOUNT_TITLE_2 = "Во сколько это обойдется? (2)";
+let expenses1, expenses2;
+let amount1, amount2;
+let amountDefaultOne = 1000;
+let amountDefaultTwo = 10000;
+let expensesDefaultOne = "Интернет";
+let expensesDefaultTwo = "Продукты";
+
+expenses1 = prompt(EXPENSES_TITLE_1, expensesDefaultOne);
+while (typeof amount1 !== "number") {
+  amount1 = parseInt(prompt(AMOUNT_TITLE_1, amountDefaultOne));
+}
+expenses2 = prompt(EXPENSES_TITLE_2, expensesDefaultTwo);
+while (typeof amount2 !== "number") {
+  amount2 = parseInt(prompt(AMOUNT_TITLE_2, amountDefaultTwo));
+}
+console.log('expenses1: ', expenses1);
+console.log('amount1: ', amount1);
+console.log('expenses2: ',expenses2) ;
+console.log('amount2: ', amount2);
+
+// Месячный бюджет
+let budgetMonth = money - amount1 - amount2;
+console.log('budgetMonth: ', budgetMonth);
+
+// Число месяцев, за которое будет накоплена сумма mission
+console.log('Months to mission: ', Math.ceil(mission / budgetMonth));
+
+// Уточненная сумма дневного бюджета
+let budgetDayRevised = Math.floor(budgetMonth / 30);
+console.log('Revised daily budget: ', budgetDayRevised);
+
+// Оценка бюджета
+const markHigh = "У вас высокий уровень дохода";
+const markAverage = "У вас средний уровень дохода";
+const markLow = "К сожалению у вас уровень дохода ниже среднего";
+const errorMessage = "Что то пошло не так";
+
+switch (true) {
+  case (budgetDayRevised >= 1200):
+    alert(markHigh);
+    break;
+  case (budgetDayRevised >= 600 && budgetDayRevised < 1200):
+    alert(markAverage);
+    break;
+ case (budgetDayRevised >= 0 && budgetDayRevised < 600):
+    alert(markLow);
+    break;
+  default:
+    alert(errorMessage);
+    break;
+}
